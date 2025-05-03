13d80681b1974bfbb2a954a081615abb73868693
ana0066 <anabelmencia06@gmail.com>
2025-05-03 19:33:16 -0400

    modificaciones del registro

---
+++ html/nosotros.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+include "../menu.php";
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Nosotros – Distribuidora Lorenzo</title>
+  <link rel="stylesheet" href="../css/style.css">
+  <link rel="stylesheet" href="../css/nosotros.css">
+  <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
+  <script src="https://www.paypal.com/sdk/js?client-id=AXUAVkqwfkvaeRrSB0AqHxi3aD8bVMnS6oOBucZteoPKfrSJ0FIKDuFlBGygkfqnNA5DRLl9ZBS902eo&buyer-country=US&currency=USD&components=buttons&enable-funding=venmo,paylater,card"></script>
+</head>
+
+<body>
+</body>
+
+</html>
